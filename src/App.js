8f9d2005a7a2e0a6d8800ba5f80291a5b38712b2
aasenaasen <aasenoscar@gmail.com>
2021-08-25 10:30:52 +0200

    first commit react starter with images and text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import header from './header.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={header} className="App-logo" alt="header" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         PROFESSIONAL WAITERS COMING TO A PARTY NEAR YOU SOON 
+        </p> 
       </header>
     </div>
   );
